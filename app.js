d9678f0e9c0e2745bec9c2d48c9f1dd883fd0673
Nurjahonbek <nurjahonbekweb@gmail.com>
2024-10-30 11:22:58 +0500

    Finetrend

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+const barsBtn = document.querySelector(".bars-btn")
+const closeBtn = document.querySelector (".left-nav-btn")
+const leftBtn = document.querySelector(".left-nav")
+
+
+barsBtn.addEventListener("click", function(){
+    leftBtn.classList.add("toggle")
+    leftBtn.classList.remove("remove")
+})
+closeBtn.addEventListener("click", function(){
+    leftBtn.classList.remove("toggle")
+    leftBtn.classList.add("remove")
+})
+
+
+
 var loader = document.querySelector(".gif")
 
 window.addEventListener("load", vanish);
